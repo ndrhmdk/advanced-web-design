47ea795a9b475301350b9b7e8e45310e7b6a4de9
ndrhmdk <andrhmdk@gmail.com>
2026-09-26 10:22:04 +0700

    npx prettier

--- session-07/square-project/app.js
+++ session-07/square-project/app.js
@@ -11,17 +11,17 @@ const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", squareRoutes);
 
 mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("MongoDB connected.");
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
-        });
-    })
-    .catch(error => {
-        console.log("MongoDB connection error:", error)
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error:", error);
+  });
--- session-07/square-project/models/square.js
+++ session-07/square-project/models/square.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const squareSchema = new mongoose.Schema({
-    sideLength: {type: Number, required: true},
-    perimeter: {type: Number, required: true},
-    area: {type: Number, required: true}
+  sideLength: { type: Number, required: true },
+  perimeter: { type: Number, required: true },
+  area: { type: Number, required: true },
 });
 
-const Square = mongoose.model('Square', squareSchema);
+const Square = mongoose.model("Square", squareSchema);
 module.exports = Square;
